ca524c3d3aa6aafbd8164468d4b06d4af82f1327
leonardo <leonardofarena@gmail.com>
2025-07-28 21:33:26 -0300

    vvsett

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     description: 'Descubre nuestros servicios de branding, diseño web, marketing digital y lanzamientos para potenciar tu marca en el mundo digital.',
     images: [
       {
-        url: '/marca-1-ar.png',
+        url: 'https://tumarca.vercel.app/marca-1-ar.png',
         width: 800,
         height: 600,
         alt: 'Tu Marca AR',
