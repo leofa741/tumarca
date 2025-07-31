a87c85d062dea6d8847704411156f25a31e29b8d
leonardo <leonardofarena@gmail.com>
2025-07-31 10:42:37 -0300

    ret

--- src/components/ValorDestacado.tsx
+++ src/components/ValorDestacado.tsx
@@ -18,7 +18,7 @@ export default function ValorDestacado({
 }: ValorDestacadoProps) {
   return (
     <div
-      className="flex items-start space-x-4 p-4 dark:bg-gray-800 rounded-lg shadow-md transition-transform transform hover:scale-105"
+      className="flex items-start space-x-4 p-4 dark:bg-gray-800 rounded-lg  transition-transform transform hover:scale-105"
       style={{ animationDelay: `${delay}s` }}
     >
       {/* Imagen */}
@@ -32,15 +32,21 @@ export default function ValorDestacado({
         />
       </div>
 
+
+
       {/* Texto */}
-      <div>
-        <h3 className="text-xl font-bold text-gray-1000 dark:text-white mb-2">
-          {titulo}
-        </h3>
-        <p className="text-gray-400 dark:text-gray-300 leading-relaxed">
-          {descripcion}
-        </p>
-      </div>
-    </div>
+        <div className="flex-1">
+            <h3 className="text-xl font-semibold text-gray-700 dark:text-white">
+            {titulo}
+            </h3>
+            <p className="text-gray-500 dark:text-gray-300 mt-2">
+            {descripcion}
+            </p>
+            <p className="text-gray-300 dark:text-gray-200 mt-2">
+              {/* Aquí puedes agregar información adicional o un llamado a la acción */}
+            </p>
+          </div>
+        </div>
+            
   );
 }
