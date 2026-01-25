a15cdd806f3a57cdd86a89eff98a9dc64e047c4d
leonardo <leonardofarena@gmail.com>
2026-01-25 10:26:17 -0300

    ch2

--- src/lib/admins.ts
+++ src/lib/admins.ts
@@ -2,6 +2,7 @@
 export const ADMIN_EMAILS = [
   "rotafelipexx1@gmail.com",
   "infoplataformaweb@gmail.com",
+  "leonardofarena@gmail.com",
 ];
 
 //699929755295-9mp16p7bpf63cvk8io8ac9341us1b5ng.apps.googleusercontent.com
